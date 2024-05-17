8a81a9824974cc435b6c26f3c7318afbe3330253
hang-up <robhang.up@gmail.com>
2024-05-17 14:49:54 -0400

    Fixed seed & auth provider.

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -1,1 +1,12 @@
-INSERT INTO public.project_types (id, name) VALUES (1, 'Audit')(2, 'Evaluation')(3, 'Evaluation Study')(4, 'Horizontal Evaluation')(5, 'OAG_Audit')(6, 'Audit Review')(7, 'OPO Audit')(8, 'PSC Audit')(9, 'Scoping Exercise')(10, 'Other');
+INSERT INTO public.project_types (id, name) 
+VALUES 
+(1, 'Audit'),
+(2, 'Evaluation'),
+(3, 'Evaluation Study'),
+(4, 'Horizontal Evaluation'),
+(5, 'OAG_Audit'),
+(6, 'Audit Review'),
+(7, 'OPO Audit'),
+(8, 'PSC Audit'),
+(9, 'Scoping Exercise'),
+(10, 'Other');
