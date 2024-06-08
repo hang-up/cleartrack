c0efd44b0c6c798957d17ee094654359fd68f9a5
hang-up <robhang.up@gmail.com>
2024-06-08 17:16:03 -0400

    Created project status enum.

---
+++ frontend/src/types/project-status.ts
@@ -0,0 +1,13 @@
+enum ProjectStatus {
+  planned = 'Planned',
+  active = 'Active',
+  on_hold = 'On Hold',
+  delayed = 'Delayed',
+  completed = 'Completed',
+  cancelled = 'Cancelled',
+  in_review = 'In Review',
+  pending_approval = 'Pending Approval',
+  not_started = 'Not Started'
+}
+
+export { ProjectStatus}
