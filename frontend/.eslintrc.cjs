04fa498391917c2d7016d0c9eff8982461dfa042
hang-up <robhang.up@gmail.com>
2024-06-08 17:12:39 -0400

    Ignore base preset from eslint.

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,6 +2,7 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  ignores: ["./src/assets/styles/BasPreset/"],
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
