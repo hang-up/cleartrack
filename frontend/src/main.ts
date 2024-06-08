4534627ee270826ae6a37d1ab7321efb00f49eb4
hang-up <robhang.up@gmail.com>
2024-06-08 17:16:35 -0400

    Added usage of primevue.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,16 @@
-import './assets/main.scss'
+import './assets/styles/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import BasePreset from "./assets/styles/BasePreset";
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.use(createPinia()).use(PrimeVue, {
+  unstyled: true,
+  pt: BasePreset
+}).use(router).mount('#app')
